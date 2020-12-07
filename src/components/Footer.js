312faa4ebe5b30ea297b5ea475aed1ecad606a38
Pravallika Pulivendula <41499315+Pravali07@users.noreply.github.com>
2020-12-07 18:33:03 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ function Footer(){
     return (
     <div style={{paddingTop:"6rem"}}>
         <nav className="navbar navbar-dark bg-danger" style={{padding:"4rem"}}>
-        <Link to="/"><a style={{color:"white", fontWeight:"200", fontSize:"16px",textDecoration:"none"}}><b>What is Todo?</b></a></Link>
-        <Link to="/"><a style={{color:"white",fontWeight:"200", fontSize:"16px",textDecoration:"none"}}><b>Why Todo?</b></a></Link>
+        <a href="./#top"style={{color:"white", fontWeight:"200", fontSize:"16px",textDecoration:"none"}}><b>What is Todo?</b></a>
+        <a href="./#top"style={{color:"white",fontWeight:"200", fontSize:"16px",textDecoration:"none"}}><b>Why Todo?</b></a>
         <Link to="/todos"><a style={{color:"white",fontWeight:"200", fontSize:"16px",textDecoration:"none"}}><b>Create Todo</b></a></Link>
         </nav>
     </div>
