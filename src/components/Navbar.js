61b8bf8cd40529e7c3f73a8f6b80ce6747d9bef5
Pravallika Pulivendula <41499315+Pravali07@users.noreply.github.com>
2020-12-07 18:34:00 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react"
 import "../App.css"
+import { Link, Redirect } from 'react-router-dom';
 class Header extends React.Component{
   render(){
     return(
         <div>
             <nav className="navbar navbar-dark bg-danger">
-            <a className="navbar-brand" style={{fontWeight:"550",height:"40px",color:"white"}}>
+            <Link to="/"><a className="navbar-brand" style={{fontWeight:"550",height:"40px",color:"white"}}>
                 <img src="./images/image1.png" class="imag1" width="30" height="30" class="d-inline-block align-top" alt=""/>    TODO
-            </a>
+            </a></Link>
             </nav>
       </div>
     )}
